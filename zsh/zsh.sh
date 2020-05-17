e8884198ca14552af8ef609536da8810d8379faa
VictorYt <victor.ythier@gmail.com>
2020-05-17 19:39:35 +0200

    zsh file update, zshrc, aliases, .sh

---
+++ zsh/zsh.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Configuration files symbolic link
+ln -fs ~/dotfiles/zsh/.aliases ~/
+ln -fs ~/dotfiles/zsh/.zshrc ~/
