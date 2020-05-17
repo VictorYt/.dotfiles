9f9cfb3c77c2245b5f5df2e258463170e20d9c8b
VictorYt <victor.ythier@gmail.com>
2020-05-17 20:20:09 +0200

    update git dofiles

---
+++ git/git.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+find ~/dotfiles/git -name '.git*' -print0 | xargs -0 -I file ln -fs file ~/
+
+git config --global core.excludesfile ~/dotfiles/git/.gitignore_global
+# Have a look to the [masOS specific .gitignre](https://github.com/github/gitignore/tree/master/Global/masOS.gitignore)
