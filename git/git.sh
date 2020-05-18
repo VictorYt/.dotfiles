3b663fe3e3e164bd18f0d3c99c00e5d905168d49
VictorYt <victor.ythier@gmail.com>
2020-05-18 17:20:37 +0200

    correcte missing dot

--- git/git.sh
+++ git/git.sh
@@ -2,5 +2,5 @@
 
 find ~/dotfiles/git -name '.git*' -print0 | xargs -0 -I file ln -fs file ~/
 
-git config --global core.excludesfile ~/dotfiles/git/.gitignore_global
+git config --global core.excludesfile ~/.dotfiles/git/.gitignore_global
 # Have a look to the [masOS specific .gitignre](https://github.com/github/gitignore/tree/master/Global/masOS.gitignore)
